0c8ecb90a17f68b5550875145846083a84772453
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-09 16:20:10 -0700

    day02: About Me

---
+++ src/day02/AboutMe.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class AboutMe {
+
+    public static void main(String[] args) {
+        // main method is the starting point of the program
+
+        System.out.println("My name is Dilshat");
+        System.out.println("I am your student for the course");
+        System.out.println("I love Java");
+    }
+}
