c055e13521612f803fc607d396e0523c84c8a279
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-09 16:16:14 -0700

    day01: First Class

---
+++ src/day01/FirstClass.java
@@ -0,0 +1,26 @@
+package day01;  // this is saying your class is under this folder, it's called package
+
+
+public class FirstClass {                 // class is the most fundamental building of your source code
+
+    public static void main(String[] args) {        //this is place your program start running
+        // write your code here
+
+        System.out.println("Hello World !!!");
+
+        String a = "Hello, my name is Dilshat";
+        String b = "I'm from California";
+        String c = "I'm a student Batch 15";
+        String d = "I'm";
+        String f = "years old";
+
+        int age = 25;
+
+        String text = a + " " + b + " " + c + " " + d + " " + age + " " + f;
+
+
+
+        System.out.println(text);
+
+    }
+}
