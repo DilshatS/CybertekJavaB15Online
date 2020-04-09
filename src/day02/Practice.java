e56d99593700c2bb1f898563085f9ff7673b1aa6
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-09 16:23:42 -0700

    day02: Practice

---
+++ src/day02/Practice.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int year = 1994;
+        int age = 25;
+        int family = 5;
+        int monitor = 267;
+        int area = 94089;
+        int salary = 100;
+
+        System.out.println("I was born " + year + " I'm " + age + " years " + "may family count " + family + "my area code " + area + " my desired salary is " + salary + " an hour" );
+
+
+    }
+
+}
+
