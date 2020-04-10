0efd5f0a370b884ca40702d0bb684bd5460901e6
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-09 20:16:59 -0700

    day02: Concatenation

---
+++ src/day02/VariableAndConcatenation.java
@@ -0,0 +1,18 @@
+package day02;
+
+public class VariableAndConcatenation {
+
+    public static void main(String[] args) {
+
+        int year = 1994;
+        int age = 25;
+        int family = 5;
+        int area = 94089;
+        int salary = 150;
+
+        System.out.println("I was born " + year + " I'm " + age + " year my family count "
+                + family + " my area code " + area);
+
+    }
+
+}
