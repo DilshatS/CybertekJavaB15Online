0813c5dfd94dcb0cedc17014a7762fe678f82872
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-09 20:24:02 -0700

    day02: Variable Intro

---
+++ src/day02/VariableIntro.java
@@ -0,0 +1,32 @@
+package day02;
+
+public class VariableIntro {
+
+    public static void main(String[] args) {
+
+        int TVSize = 67;
+        // name is
+
+        System.out.println(TVSize);
+
+        // TASK 1
+        // name your favorite number
+        // name your classmate count
+
+        int favoriteNumber = 21;
+        int calssMateCount = 271;
+
+        // int classmateCount = 271; Variable name can not be reused!!!
+
+        int nextNumberFrom9 = 10;
+        int populationOfBatch15Now = 300;
+
+        // int 9 to 5 = 95 number can not come first in any line
+
+        int myFolderCount = 6;
+
+        System.out.println(nextNumberFrom9);
+
+    }
+
+}
