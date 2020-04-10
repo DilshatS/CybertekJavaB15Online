30b4e3e22e486ab7bfc810e8fed0011ea4fdba62
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-09 21:32:27 -0700

    day03: Print Vs Println (\")

---
+++ src/day03/PrintVsPrintln.java
@@ -0,0 +1,23 @@
+package day03;
+
+public class PrintVsPrintln {
+
+    public static void main(String[] args) {
+
+        // 3 print line statement
+        System.out.println("Hello world line 1");
+        System.out.println("Hello world line 2");
+        System.out.println("Hello world line 3");
+        // 2 print statement
+        System.out.print("Hello world line 4");
+        System.out.print("Hello world line 5");
+        // 2 print line statement
+        System.out.println("Hello world line 6");
+        System.out.println("Hello world line 7");
+
+        System.out.println("Hello \"world\"");
+
+
+    }
+
+}
