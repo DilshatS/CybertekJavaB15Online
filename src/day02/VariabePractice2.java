c090781f800df27b6de62101bb757f582b150410
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-09 20:28:32 -0700

    day02: Variable Practice 2

---
+++ src/day02/VariabePractice2.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class VariabePractice2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("My program started");
+
+        int my$FolderCount;
+
+        my$FolderCount = 10;
+
+        System.out.println(my$FolderCount);
+
+        my$FolderCount = 100;
+
+        System.out.println(my$FolderCount);
+
+    }
+
+}
