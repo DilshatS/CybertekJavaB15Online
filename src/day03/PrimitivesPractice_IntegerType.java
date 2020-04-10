8d6ef27add87e9fc98c8eb16221738a6b33e3805
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-09 22:16:36 -0700

    day03: Primitives Practice Integer Type

---
+++ src/day03/PrimitivesPractice_IntegerType.java
@@ -0,0 +1,27 @@
+package day03;
+
+public class PrimitivesPractice_IntegerType {
+
+    public static void main(String[] args) {
+
+        byte letterCount = 26;
+        System.out.println("The letter count is " + letterCount);
+
+        short sheepCount = 300;
+        System.out.println("sheepCount = " + sheepCount);
+
+        int catCount = 20;
+        System.out.println("catCount = " + catCount);
+
+        long mileToMoon = 5000000L;
+        System.out.println("mileToMoon = " + mileToMoon);
+
+        float priceOfBanana = 1.99f;
+        System.out.println("priceOfBanana = " + priceOfBanana);
+
+        double priceOfIpad1 = 355.99d;
+        System.out.println("priceOfIpad1 = " + priceOfIpad1);
+
+    }
+
+}
