23a10a7dfaa7db1cbceb7f1cae7052e68ede573e
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-10 23:13:00 -0700

    day03: String Practice

---
+++ src/day04/StringPractice.java
@@ -0,0 +1,19 @@
+package day04;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+
+        String firstName = "Dilshat";
+        String lastName = "Sadamov";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("my fist name is " + firstName);
+        System.out.println("my last name is " + lastName);
+
+        System.out.println("my full name is " + fullName);
+
+    }
+
+}
