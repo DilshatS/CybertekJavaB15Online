67f1433f75e62672cad82c8242ddb1d4f3cd878b
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-10 23:23:24 -0700

    day03: Practice Amazon Price

---
+++ src/day04/AmazonPractice.java
@@ -0,0 +1,23 @@
+package day04;
+
+public class AmazonPractice {
+
+    public static void main(String[] args) {
+
+        // String name Amazon Fire
+        // String model HD
+        // int version 8
+        // float price
+
+        // I saw Fire HD8 hands-free with Alexa for $79.99
+        String productName = "Amazon Fire";
+        String model = "HD";
+        int version = 8;
+        float price = 79.99f;
+
+        System.out.println("I saw " + productName + " " + model + version +
+                " \nhands-free with Alexa \nfor $" + price);
+
+    }
+
+}
