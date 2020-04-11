5f334a54e0d7965c72c985c2212d3ed3291cd9c6
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-10 18:41:25 -0700

    day03: Character and boolean

---
+++ src/day03/CharacterAndBoolean.java
@@ -0,0 +1,25 @@
+package day03;
+
+public class CharacterAndBoolean {
+
+    public static void main(String[] args) {
+
+        char grade = 'A';
+        char emailSign = '@';
+
+        System.out.println("My score for Math is " + grade);
+
+        boolean isLightOn = true;
+        System.out.println("did you turn off the light? : " + isLightOn);
+        isLightOn = false;
+        System.out.println("did you turn off the light? : " + isLightOn);
+
+
+        // boolean areYouSleeping = false;
+        // System.out.println("are you sleeping? : " + areYouSleeping);
+
+
+
+    }
+
+}
