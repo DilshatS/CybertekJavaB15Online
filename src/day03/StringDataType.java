dc1904d6cfb50c85c9e9f28b5235c19bc4cb5f6b
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-10 23:01:20 -0700

    day03: String data type

---
+++ src/day03/StringDataType.java
@@ -0,0 +1,37 @@
+package day03;
+
+public class StringDataType {
+
+    public static void main(String[] args) {
+
+        String name = "Dilshat";
+        int age = 25;
+        double height = 5.11;
+        char myLastNameFirstChar = 'D';
+        boolean isMarried = true;
+        byte childrenCount = 2;
+        String city = "Tyson";
+
+        System.out.println(name);
+
+        System.out.println("My name is : " + name);
+        System.out.println("My age is : " + age);
+        //System.out.println("My height is : " + height);
+        System.out.println("My my Last Name First Character is : " + myLastNameFirstChar);
+        System.out.println("I am Married : " + isMarried);
+        System.out.println("I have  : " + childrenCount);
+        System.out.println("I live in : " + city);
+        // continue the rest about yourself
+
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("My name is : " + name + ", My age is : " + age +
+                ", My my Last Name First Character is : " + myLastNameFirstChar +
+                ", I am Married : " + isMarried + "I have  : " + childrenCount +
+                ", I live in : " + city);
+
+
+
+    }
+
+}
