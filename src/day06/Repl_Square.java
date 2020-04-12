77972c5ec1d5e79a1413fe503b7265b68b3849a5
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-11 23:38:50 -0700

    day06: Repl Square Task

---
+++ src/day06/Repl_Square.java
@@ -0,0 +1,18 @@
+package day06;
+
+public class Repl_Square {
+
+    public static void main(String[] args) {
+
+        System.out.println("* * * * * * * *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("* * * * * * * *");
+
+    }
+
+}
