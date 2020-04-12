8df9cc2562b575c6fdc7553bd7acb8d82cce81ad
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-11 23:39:23 -0700

    day06: Scanner Capturing Multiple Word

---
+++ src/day06/ScannerCapturingMultipleWord.java
@@ -0,0 +1,23 @@
+package day06;
+
+import java.util.Scanner;
+
+public class ScannerCapturingMultipleWord {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your name? : ");
+        String name = scan.next(); // it will only capture first word (separated by space
+
+        //String anotherWord = scan.next();
+        int age = scan.nextInt();
+
+        System.out.println("You have entered " + name);
+        //System.out.println("Another word is " + anotherWord);
+        System.out.println("You have entered age " + age);
+
+    }
+
+}
