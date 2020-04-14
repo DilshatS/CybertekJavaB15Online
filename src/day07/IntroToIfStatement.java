5c9cb4b7d0a9b1803c63484964bfca66ec1a46a0
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-13 22:10:19 -0700

    day07:If Statement Intro

---
+++ src/day07/IntroToIfStatement.java
@@ -0,0 +1,23 @@
+package day07;
+
+public class IntroToIfStatement {
+
+    public static void main(String[] args) {
+
+        int score = 70;
+
+        // if( PUT YOUR CONDITION HERE) {
+
+        // do this if the condition is true
+
+        // }
+
+        System.out.println( score > 60 );
+
+        if ( score > 60 ){
+            System.out.println("Chocolate for you!!!");
+        }
+        System.out.println("THE END");
+    }
+
+}
