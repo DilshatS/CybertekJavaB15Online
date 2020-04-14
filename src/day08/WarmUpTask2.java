01ced071ca6baddb4080d32157481a7dfefa78ed
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-13 23:17:53 -0700

    day07: Warm up task 2 TRY GUESS MY FAVORITE NUMBER

---
+++ src/day08/WarmUpTask2.java
@@ -0,0 +1,30 @@
+package day08;
+
+import java.util.Scanner;
+
+public class WarmUpTask2 {
+
+    public static void main(String[] args) {
+
+        /*
+        Create a program to guess your favorite number
+        create a variable called myFavoriteNumber as int
+        Ask user to guess your favorite number
+        if user guessed right then print BINGO!!!
+        if not print try again later
+         */
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("GUESS MY FAVORITE NUMBER : ");
+        int myFavoriteNumber = scan.nextInt();
+
+        if ( myFavoriteNumber == 300 ){
+            System.out.println("BINGO!!!");
+        }else {
+            System.out.println("You are not my best Friend, try again");
+        }
+
+    }
+
+}
