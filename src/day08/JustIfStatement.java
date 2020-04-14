e61b5d194f47cbad5b40e75fe6cdaf71697ffc48
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-14 16:37:45 -0700

    day07: only If Statement with Scanner

---
+++ src/day08/JustIfStatement.java
@@ -0,0 +1,27 @@
+package day08;
+
+import java.util.Scanner;
+
+public class JustIfStatement {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your current speed? : ");
+
+        int speed = scan.nextInt();
+        System.out.println("What is speed limit? : ");
+
+        int speedLimit = scan.nextInt();
+
+        if (speed >= speedLimit){
+            System.out.println("get pulled over by the police");
+            System.out.println("given ticket by the police");
+            System.out.println("taken away some points on your license");
+            System.out.println("go to court");
+        }
+        // there is no else, program just move on
+        System.out.println("The end of the story! Move on!");
+    }
+
+}
