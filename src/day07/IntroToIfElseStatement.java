204ed9dad9f50269efb4de9275030c5fff7256e0
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-13 22:10:51 -0700

    day07:If Else Statement Intro

---
+++ src/day07/IntroToIfElseStatement.java
@@ -0,0 +1,27 @@
+package day07;
+
+public class IntroToIfElseStatement {
+
+    public static void main(String[] args) {
+
+        int score = 60;
+
+        // if( PUT YOUR CONDITION HERE) {
+
+        // do this if the condition is true
+
+        // }else{
+        //         do this if it not true
+        // }
+
+        System.out.println(score >= 60);
+
+        if (score > 60) {
+            System.out.println("Chocolate for you!!!");
+        } else {
+            System.out.println("Try harder next time! no TV, no PHONE, DO NOT GIVE UP!!!");
+        }
+        System.out.println("THE END");
+    }
+
+}
