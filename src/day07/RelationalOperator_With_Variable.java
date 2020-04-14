34e2aea6349c0b7b1b746d3ae27de794e1474464
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-13 22:08:58 -0700

    day07: Relational Operator with variable

---
+++ src/day07/RelationalOperator_With_Variable.java
@@ -0,0 +1,37 @@
+package day07;
+
+public class RelationalOperator_With_Variable {
+
+    public static void main(String[] args) {
+
+        // >, >=, <, <=, ==, != Relational Operator
+
+        // Relation Operator is used to compare 2 operands
+        // and return true or false
+        // anything action or expression that return true or false result
+        // can be called boolean expression
+        int myNumber = 10;
+
+        System.out.println(myNumber > 5 );
+        System.out.println(myNumber <- 19 );
+        System.out.println(myNumber >= 5 );
+        System.out.println(myNumber >= 8 );
+        System.out.println(myNumber <= 5 );
+        System.out.println(myNumber >= 5 );
+
+        //  == is used to check equality
+        // AND VERY DIFFERENT FROM SINGLE
+
+        // difference between == and = same as
+        // difference between equal and equality daily English
+        System.out.println(myNumber == 5 );
+
+        // != is used to check for inequality, ! in programming translate to NOT
+        System.out.println(myNumber != 10 );
+        System.out.println(myNumber != 6 );
+
+        // == and != are questions, checking for equality or inequality
+
+    }
+
+}
