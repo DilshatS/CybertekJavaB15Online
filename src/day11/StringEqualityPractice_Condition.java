76c9f3fd0928c036db12f74bd739b07c87e724ac
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-16 00:53:49 -0700

    day11: String equality Practice "Java"

---
+++ src/day11/StringEqualityPractice_Condition.java
@@ -0,0 +1,22 @@
+package day11;
+
+public class StringEqualityPractice_Condition {
+
+    public static void main(String[] args) {
+
+        // Create a variable called myStr and assign a value
+        // check whether myStr value is Java
+        // if true --> CORRECT WORD!!!
+        // if false --> SAY JAVA!!!
+
+        String myStr = "Java";
+
+        if (myStr.equals("Java")){
+            System.out.println("CORRECT WORD!!!");
+        }else {
+            System.out.println("SAY JAVA!!!");
+        }
+
+    }
+
+}
