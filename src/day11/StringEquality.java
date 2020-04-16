ffb48b53cf932cc6f56e9d77b115328f4808895d
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-15 23:45:21 -0700

    day11: Primitive equality check

---
+++ src/day11/StringEquality.java
@@ -0,0 +1,30 @@
+package day11;
+
+public class StringEquality {
+
+    public static void main(String[] args) {
+
+        //String name = "Rabia";
+
+        char c1 = 'b';
+        char c2 = 'B';
+        char c3 = 'b';
+
+        System.out.println("is c1 equal to c2");
+        System.out.println( c1 == c2 );
+
+        System.out.println("is c1 equal to c3");
+        System.out.println( c1 == c3 );
+
+        // all primitive types can be compared using == for their content equality
+        // primitive types are pure value
+
+        int num1 = 10;
+        int num2 = 10;
+
+        System.out.println("is num1 equal to num2");
+        System.out.println( num1 == num2 );
+
+    }
+
+}
