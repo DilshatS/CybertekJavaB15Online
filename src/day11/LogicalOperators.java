860d73cee3a859b404908fb858534bf561021c46
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-15 21:45:32 -0700

    day11: Logical Operators && || more examples

--- src/day11/LogicalOperators.java
+++ src/day11/LogicalOperators.java
@@ -71,16 +71,38 @@ public class LogicalOperators {
         // and print out the result
 
         int x = 55;
+        System.out.println("------- x > 100 || x < 10 -----");
         System.out.println( x > 100 || x < 10 );
         // System.out.println(10 < x < 60);// error! Can not be applied to boolean, int
+
+        System.out.println("-------  x > 10 && x < 60 -----");
         System.out.println( x > 10 && x < 60 );
 
         // more than 2 conditions at the same time, 3 conditions
 
-        //                        true || false ---> true
+        System.out.println("result of true || false || false");
         System.out.println( true || false || false);
-        //                        true && false ---> false
+
+        System.out.println("result of true && true && false");
+        //                       true && false --> false
         System.out.println( true && true && false );
+
+        System.out.println("running this ---> x == 50 || x == 51 || x == 55");
+        System.out.println( x == 50 || x == 51 || x == 55 );
+
+        // == is checking for equality, != is checking for inequality
+
+        System.out.println("running this ---> x > 50 && x != 52 || x == 57");
+        // check
+        // x is more than 50
+        // and x is not equal to 52
+        // or x equal to 57
+
+        // check x is more than 50 and x is not 52 or x equal to 57
+        //                    true && true -> true || false --> true
+        System.out.println( x > 50 && x != 52 || x == 57 );
+
+        System.out.println( false || true && true );
 
     }
 
