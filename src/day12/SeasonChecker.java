b1e80017ac1b8cc989e765d0c365e4a4f49a2aee
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-16 18:45:03 -0700

    day12: Login test, username and password checker checking for equality equalsIgnoreCase, upper/lower cases

---
+++ src/day12/SeasonChecker.java
@@ -0,0 +1,39 @@
+package day12;
+
+import java.util.Scanner;
+
+public class SeasonChecker {
+
+    public static void main(String[] args) {
+
+        /**
+         * Task 5
+         * Create a String variable season
+         * Assign value by asking user question: Enter your season Please!
+         *
+         * Check the season using if statement without caring about the case
+         * Spring ---> Hiking, Easter , Nauriz, Blossom
+         * Summer ---> Swim, Vacation, BBQ, Holiday
+         * Fall ---> Black Friday, Hiking, Harvest, Halloween, Shopping
+         * Winter ---> Code, Ski, Christmas, New Year, Snowman
+         */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your season Please!");
+
+        String season = scan.next();
+
+        if (season.equalsIgnoreCase("spring")){
+            System.out.println("Hiking, Easter , Nauriz, Blossom");
+        }else if (season.equalsIgnoreCase("summer")) {
+            System.out.println("Swim, Vacation, BBQ, Holiday");
+        }else if (season.equalsIgnoreCase("fall")) {
+            System.out.println("Black Friday, Hiking, Harvest, Halloween, Shopping");
+        }else if (season.equalsIgnoreCase("winter")) {
+            System.out.println("Code, Ski, Christmas, New Year, Snowman");
+        }else{
+            System.out.println("NOT VALID!!!");
+        }
+    }
+
+}
