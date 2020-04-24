d4f2b66304a00496736519ab7f21f8a4f36e0d0e
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-24 15:56:17 -0700

    day16: String Practice Getting words of the sentence

--- src/day16/SecondWordGetter.java
+++ src/day16/SecondWordGetter.java
@@ -15,13 +15,14 @@ public class SecondWordGetter {
         // remind me of substring
         //                 01234567890
         String sentence = "You Understand Java";
+        System.out.println("sentence = " + sentence);
         String secondWord;
         int firstSpaceIndex = sentence.indexOf(" ");
         System.out.println("firstSpaceIndex = " + firstSpaceIndex);
 
         int lastSpaceIndex = sentence.lastIndexOf(" ");
         System.out.println("lastSpaceIndex = " + lastSpaceIndex);
-        System.out.println(sentence.substring(1+1,6));
+        //System.out.println(sentence.substring(1+1,6));
         secondWord = sentence.substring(firstSpaceIndex+1,lastSpaceIndex);
         System.out.println("secondWord = " + secondWord);
 
@@ -31,6 +32,12 @@ public class SecondWordGetter {
         String firstWord = sentence.substring(0,firstSpaceIndex);
         System.out.println("firstWord = " + firstWord);
         // how do we print out a variable with value using shortcut : soutv then tab
+
+        // how to get last word from any sentence
+        // last word in a sentence means from last space till the rest
+
+        String lastWord = sentence.substring(lastSpaceIndex+1);
+        System.out.println("lastWord = " + lastWord);
 
     }
 
