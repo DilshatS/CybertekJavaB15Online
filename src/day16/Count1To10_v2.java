d7c3c8a23cf579634065b5bcc57e4ed8643bdcf8
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-29 13:07:33 -0700

    day16: Loop, While Loop Count to 10 in different ways

---
+++ src/day16/Count1To10_v2.java
@@ -0,0 +1,36 @@
+package day16;
+
+public class Count1To10_v2 {
+
+    public static void main(String[] args) {
+
+//        int counter = 0;
+//
+//        while ( counter < 10){
+//
+//            // ++ counter --> counter = counter + 1
+//            System.out.println(++counter);
+//
+//        }
+
+//        int counter = 1;
+
+//        while (counter <= 10){
+//
+//            System.out.println(counter++);
+//        }
+
+//        int counter = 0;
+//
+//        while( ++counter <= 10 ){
+//            System.out.println(counter);
+//        }
+
+        int counter = 0;
+        while ( counter++ < 10 ){
+            System.out.println(counter);
+        }
+
+    }
+
+}
