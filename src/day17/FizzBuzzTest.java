5702bbab1ebbd1249279893aaa9411d31a7bbdbf
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-29 18:12:25 -0700

    day17: While Loop | Fizz Buzz Test for 0 - 100

--- src/day17/FizzBuzzTest.java
+++ src/day17/FizzBuzzTest.java
@@ -9,14 +9,16 @@ public class FizzBuzzTest {
         // if a number can be divided by 3 --> Buzz number
 
         int num = 1;
-        while (num <= 100) {
+        while (num <= 47) {
 
             if (num % 5 == 0 && num % 3 == 0) {
-                System.out.println(num + " is Fizz Buzz Number");
+                System.out.println(num + " : is Fizz Buzz Number");
             } else if (num % 5 == 0) {
-                System.out.println(num + " Fizz Number");
+                System.out.println(num + " : Fizz Number");
             } else if (num % 3 == 0) {
-                System.out.println(num + " Buzz Number");
+                System.out.println(num + " : Buzz Number");
+            }else {
+                System.out.println(num + " : NOT FIZZ BUZZ NUMBER");
             }
             ++num;
         }
