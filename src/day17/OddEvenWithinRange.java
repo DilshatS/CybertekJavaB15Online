e08248e94e8713e761cd18aacaffddf6127585e9
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-29 17:25:55 -0700

    day17: While Loop | Odd or Even Number

--- src/day17/OddEvenWithinRange.java
+++ src/day17/OddEvenWithinRange.java
@@ -7,7 +7,6 @@ public class OddEvenWithinRange {
         // write a program to print out a even number from 0 to 50
         // write a program to print out a odd number from 0 to 50
 
-        System.out.println();
         int count = 0;
         while(count <= 50){
             System.out.print(count + " ");
@@ -16,9 +15,30 @@ public class OddEvenWithinRange {
 
         System.out.println();
         int count2 = 1;
-        while (count2 < 50){
+        while (count2 <= 50){
             System.out.print(count2 + " ");
             count2 += 2;
+
+        }
+        System.out.println();
+        int count3 = 0;
+
+//        if (count3 %2 == 0){
+//            System.out.println(count3 + " is even number");
+//        }else {
+//            System.out.println(count3 + " is odd number");
+//        }
+
+        while (count3 <= 50){
+
+            if ( count3 %2 == 0){
+                System.out.println(count3 + " is even number");
+            }else {
+                System.out.println(count3 + " is odd number");
+            }
+
+            ++count3;
+
         }
 
     }
