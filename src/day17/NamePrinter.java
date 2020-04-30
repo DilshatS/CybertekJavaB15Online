87b7c5d9cda8f6728fa4e25399342851864747e1
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-29 18:55:09 -0700

    day17: While Loop | Name Printer Without Loop

---
+++ src/day17/NamePrinter.java
@@ -0,0 +1,43 @@
+package day17;
+
+public class NamePrinter {
+
+    public static void main(String[] args) {
+
+        // if you have complain in life : call 1-800-Alexander
+        String name = "Alexander";
+
+        // get each and every character and print them out vertically
+        int x = 0;
+        System.out.println(name.charAt(x));
+        ++x;
+        System.out.println(name.charAt(x));
+        ++x;
+        System.out.println(name.charAt(x));
+        ++x;
+        System.out.println(name.charAt(x));
+        ++x;
+        System.out.println(name.charAt(x));
+        ++x;
+        System.out.println(name.charAt(x));
+        ++x;
+        System.out.println(name.charAt(x));
+        ++x;
+        System.out.println(name.charAt(x));
+        ++x;
+        System.out.println(name.charAt(x));
+
+
+//        System.out.println(name.charAt(0));
+//        System.out.println(name.charAt(1));
+//        System.out.println(name.charAt(2));
+//        System.out.println(name.charAt(3));
+//        System.out.println(name.charAt(4));
+//        System.out.println(name.charAt(5));
+//        System.out.println(name.charAt(6));
+//        System.out.println(name.charAt(7));
+//        System.out.println(name.charAt(8));
+
+    }
+
+}
