b301ed1f4358212141b365822a2c4dc693cbe4e5
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-30 13:44:15 -0700

    day18: While Loop Quick Review

---
+++ src/day18/WhileLoopReview.java
@@ -0,0 +1,16 @@
+package day18;
+
+public class WhileLoopReview {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        while ( x > 5){
+            System.out.println(x + " ITS MORE THAN 5");
+            --x;
+        }
+        System.out.println("THE END");
+    }
+
+}
