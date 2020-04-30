eeaa06c7caa2d96d9190dd14f669e06f766fda17
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-04-30 14:22:14 -0700

    day18: While Loop Quick Review with Scanner and String

---
+++ src/day18/SmartLockOpener.java
@@ -0,0 +1,30 @@
+package day18;
+
+import java.util.Scanner;
+
+public class SmartLockOpener {
+
+    public static void main(String[] args) {
+
+        // smart lock door has password
+        // unless you enter correct password
+        // it keep locking you out
+        // assuming correct password is B15
+        // keep asking until user enter correct one
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("KNOCK KNOCK");
+        System.out.println("WHAT IS THE PASSWORD?");
+
+        // entering password for the first time
+        String password = scan.next();
+
+        while (!password.equals("B15")){
+            System.out.println("WRONG PASSWORD!!! TRY AGAIN!!!");
+            password = scan.next();
+        }
+        System.out.println("OPEN SESAME!");
+    }
+
+}
