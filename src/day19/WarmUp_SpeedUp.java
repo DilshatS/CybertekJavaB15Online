60fbba175e556ebefdf80ca36e71c5a82325547f
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-01 16:34:23 -0700

    day19: Warm Up Task 1

---
+++ src/day19/WarmUp_SpeedUp.java
@@ -0,0 +1,21 @@
+package day19;
+
+public class WarmUp_SpeedUp {
+
+    public static void main(String[] args) {
+
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(i + " ");
+//        }
+
+        int start = 5;
+        int end = 19;
+
+        System.out.print("you have started at speed --> ");
+        for (int i = start; i <= end; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+}
