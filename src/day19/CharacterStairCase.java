fa35ac9d5c4e03e87ed0db57bb3302251fe48d28
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-04 14:53:15 -0700

    day19: For Loop With A-Z Stair Case

---
+++ src/day19/CharacterStairCase.java
@@ -0,0 +1,15 @@
+package day19;
+
+public class CharacterStairCase {
+
+    public static void main(String[] args) {
+
+        String line = "";
+        for (char iChar = 'A'; iChar <= 'Z'; iChar++) {
+            line += iChar;
+            System.out.println(line);
+        }
+
+    }
+
+}
