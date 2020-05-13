a3ea8dbc084e2e21ff68c9a0bf75755c1cc97001
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-13 09:34:45 -0700

    day21: Searching words with 3 characters in a sentence

---
+++ src/day21/CountSomethingInString.java
@@ -0,0 +1,11 @@
+package day21;
+
+public class CountSomethingInString {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
--- src/day21/SearchingSomethingInString3.java
+++ src/day21/SearchingSomethingInString3.java
@@ -4,6 +4,18 @@ public class SearchingSomethingInString3 {
 
     public static void main(String[] args) {
 
+        // Find out index all the an in case insensitive manner
+        // I will go through each and every letter of the String by 2 character
+        // --> each character I can use substring(0,2) and so on
+        // --> going to 0 to one character before last char index --> for loop
+        // while I am going through each and every 2 character
+        // I will check whether current String I am looking at
+        // equals to <an> in case insensitive --> currentCharacter.equalsIgnoreCase("an");
+        // if it is I will print index println(the variable you use to keep the index)
+        // if not --> just move on
+        // perform this action until I reach one character before last character
+        // because last chance to get 2 characters is --> right before last character
+
         String myName = "Amelia Israfil Hajitev Mehmet Hajitev Amelia Behlia Ashr";
 
         int charCount = myName.length();
