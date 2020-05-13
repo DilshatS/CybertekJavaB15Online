61d0bd08472fb860eea790f8e32cbe29d6a874b1
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-13 09:25:14 -0700

    day21: Searching words with different character count in a sentence

--- src/day21/SearchingSomethingInString.java
+++ src/day21/SearchingSomethingInString.java
@@ -4,37 +4,68 @@ public class SearchingSomethingInString {
 
     public static void main(String[] args) {
 
-        // Find out index all the a in case insensitive manner
-        // I will go through each and every letter of the String
-        // --> each character I can use substring(0,1) and so on
-        // --> going to 0 to last index --> for loop
-        // while I am going through each and every character
+        /*
+        Given your name stored in a variable myName as String
+        for example: String myName = "My name is YourName here";
 
-        // I will check whether current character I am looking at
-        // equals to a or A --> currentCharacter.equalsIgnoreCase("a");
-        // if it is I will print index println(the variable you use to keep the index)
-        // if not --> just move on
-        // perform this action until I reach last character if I go over last character index I stop
+        1. loop through each and every character print them out like this: M->y-> n->a->m->e and so on
+        put arrow in between the character
+        I chalenge you to use substring for this task
+         */
 
-        String myName = "Sebastian German";
+        //               012345678901234567890123
+        String myName = "Gulay Dimerel";
 
         int charCount = myName.length();
         System.out.println("charCount = " + charCount);
         int lastCharIndex = charCount-1;
         System.out.println("lastCharIndex = " + lastCharIndex);
 
-        System.out.println();
+//        01234
+//        Hello
+//        int x = 0;
+//        System.out.println(myName.substring(x,x+1));  H
+//        x++; 1
+//        System.out.println(myName.substring(x,x+1));  e
+//        x++; 2
+//        System.out.println(myName.substring(x,x+1));  l
+//        x++; 3
+//        System.out.println(myName.substring(x,x+1));  l
+//        x++; 4
+//        System.out.println(myName.substring(x,x+1));  0
+
+//        for hello example : there is two way to represent when do we keep looping
+//        if the x value is less than or equal to 4, x <= 4
+//        if the x value is less than 5, x < 5
 
         //for (int x = 0; x < charCount; x++) {
-        for(int x = 0; x <= lastCharIndex; x++){
-            String currentChar = myName.substring(x,x+1);
+        for (int x = 0; x <= lastCharIndex; x++) {
 
-            if (currentChar.equalsIgnoreCase("a")){
+            //System.out.print( myName.charAt(x)+ "->" );
+            System.out.print(myName.substring(x,x+1)+"->");
 
-                System.out.println("The index of a or A is " + x);
-
-            }
         }
+
+        System.out.println();
+
+        for (int x = 0; x <= lastCharIndex-1; x++) {
+        //for (int x = 0; x < charCount-2; x++) {
+
+            String twoChar = myName.substring(x,x+2);
+            System.out.print(twoChar + "->");
+
+        }
+
+        System.out.println();
+
+        for (int x = 0; x <= lastCharIndex-2; x++) {
+            //for (int x = 0; x < charCount-2; x++) {
+
+            String twoChar = myName.substring(x,x+3);
+            System.out.print(twoChar + "->");
+
+        }
+
 
     }
 
