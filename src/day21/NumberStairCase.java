67164cd2734089e79b10a67875874516423c6e36
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-13 17:25:42 -0700

    day21: Nested Loop Number StairCase

---
+++ src/day21/NumberStairCase.java
@@ -0,0 +1,59 @@
+package day21;
+
+public class NumberStairCase {
+
+    public static void main(String[] args) {
+
+        /*
+        1234 count from 1 to 4
+        1234 count from 1 to 4
+        1234 count from 1 to 4
+        1234 count from 1 to 4
+        1234 count from 1 to 4
+
+        do this 5 times
+         */
+
+        for (int x = 1; x <= 5; x++) {
+            //System.out.println("x :" + +5);
+
+            for (int y = 1; y <= x; y++) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+//
+//            for (int y = 1; y <= 4; y++) {
+//                System.out.print(y + " ");
+//            }
+//
+//            System.out.println();
+//        }
+
+//            for (int y = 1; y <= 1; y++) {
+//                System.out.print(y + " ");
+//            }
+//
+//            System.out.println();
+//
+//            for (int y = 1; y <= 2; y++) {
+//                System.out.print(y + " ");
+//            }
+//
+//            System.out.println();
+//
+//            for (int y = 1; y <= 3; y++) {
+//                System.out.print(y + " ");
+//            }
+//
+//            System.out.println();
+//
+//            for (int y = 1; y <= 4; y++) {
+//                System.out.print(y + " ");
+//            }
+//
+//            System.out.println();
+
+        }
+
+    }
+}
