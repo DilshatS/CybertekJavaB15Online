b3f4b3d0e53e47644b9bd03788265078899e9e6a
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-15 21:22:19 -0700

    day22: String array practice

---
+++ src/day22/StringArray.java
@@ -0,0 +1,25 @@
+package day22;
+
+public class StringArray {
+
+    public static void main(String[] args) {
+
+        // create an String array with ize that you define
+        // and store all your household member names
+
+        String[] lovedOnes = new String[5];
+        lovedOnes[0] = "Ned Stark";
+        lovedOnes[1] = "Catlin Stark";
+        lovedOnes[2] = "Sansa Stark";
+        lovedOnes[3] = "Arya Stark";
+        lovedOnes[4] = "Rob Stark";
+
+        System.out.println(lovedOnes[0]);
+        System.out.println(lovedOnes[1]);
+        System.out.println(lovedOnes[2]);
+        System.out.println(lovedOnes[3]);
+        System.out.println(lovedOnes[4]);
+
+    }
+
+}
