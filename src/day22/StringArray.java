35a64136c9fd583ed00f2e90988eec0ce9a0bbb8
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-15 21:34:52 -0700

    day22: String array practice

--- src/day22/StringArray.java
+++ src/day22/StringArray.java
@@ -20,6 +20,15 @@ public class StringArray {
         System.out.println(lovedOnes[3]);
         System.out.println(lovedOnes[4]);
 
+        // immutable means you can not change the internal structure of the object
+        // if you have String abc = "Hello";
+        // if you do abc.toUppercase --> it will create new String object
+        // and it does not change original hello at all!!!
+
+        // not resizable --> specifically for array
+        // we can not change the elements inside this array
+        // and it means : IT IS NOT IMMUTABLE
+
     }
 
 }
