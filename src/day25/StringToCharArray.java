efb4531261f01396b59383cbab8f4ded4d824809
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-20 03:10:30 -0700

    day22: String to char array with toCharArray then print with for each loop

--- src/day25/StringToCharArray.java
+++ src/day25/StringToCharArray.java
@@ -1,12 +1,26 @@
 package day25;
 
+import java.util.Arrays;
+
 public class StringToCharArray {
 
     public static void main(String[] args) {
 
         // pick up your own name and turn in into char array
         // and use for each loop to loop over them
-        // OPTIONALLY count how many you have in your name
+        // OPTIONALLY count how many you a have in your name
+
+        String name = "Dilshat";
+        // turn this into charArray using toCharArray() method of String
+
+        char[] allCharsInName = name.toCharArray();
+        //System.out.println("namesChar = " + Arrays.toString(allCharsInName));
+
+        for (char eachChar : allCharsInName) {
+
+             System.out.println("eachChar = " + eachChar);
+
+        }
 
     }
 
