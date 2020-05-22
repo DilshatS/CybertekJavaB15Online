97f19645168c59fe0db5ab57e1e8669f4d500dbe
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-22 16:39:59 -0700

    day26: Number more than 100 in array test logic 1.2

--- src/day26/ArrayTask_NumberTest.java
+++ src/day26/ArrayTask_NumberTest.java
@@ -12,37 +12,34 @@ public class ArrayTask_NumberTest {
          */
 
         int[] scores = {156,101,76,187,87,110};
+        int size = scores.length;
+        System.out.println("size = " + size);
 
         // create a variable called finalResult ro store
         // your result of checking if every number in this array is more than 100 or not
 
         String finalResult = ""; // if all numbers are more than 100 then make it yes, if not make it no
 
-        // LOGIC 1:
-        // take each item check if it is less than 100, count in the end if count is more than 0, answer is NO
+        // LOGIC 1.2:
+        // take each item check if it is MORE than 100, increase the count.
+        // in the end if count is equal to array item count the finalResult Yes, answer is NO
 
         int cntLessThan100 = 0;
         for( int eachNum : scores ){
 
+            if (eachNum > 100){
 
-            if (eachNum > 100 ){
-
-                System.out.println("eachNum = " + eachNum + " YES");
-                cntLessThan100++;
-            }else if (eachNum < 100){
-
-                System.out.println("eachNum = " + eachNum + " NO");
+                ++cntLessThan100;
 
             }
 
         }
-
-        System.out.println("count YES : " + cntLessThan100);
-
-        if (cntLessThan100 > 0){
-            finalResult = "NO";
-        }else {
+        System.out.println("cntLessThan100 = " + cntLessThan100);
+        // if all the numbers are more than 100, it will have same count as item count
+        if ( cntLessThan100 == size ){
             finalResult = "YES";
+        }else {
+            finalResult = "NO";
         }
 
         System.out.println("finalResult = " + finalResult);
