8f088715ec887b406ab944575e33d701c043eeb9
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-29 16:45:53 -0700

    day 29: Filling up array with items

---
+++ src/day29/FillingUpArrayValue.java
@@ -0,0 +1,30 @@
+package day29;
+
+import java.util.Arrays;
+
+public class FillingUpArrayValue {
+
+    public static void main(String[] args) {
+
+        // create an array with size 100 and fill it up as below
+
+        int[] numbers = new int[100];
+
+        System.out.println("Before filling up \n" + Arrays.toString(numbers));
+
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+//        numbers[2] = 3;
+//        ...
+//        numbers[99] = 100;
+
+        for (int x = 0; x < numbers.length; x++) {
+
+            numbers[x] = x+1;
+
+        }
+        System.out.println("After filling up \n" + Arrays.toString(numbers));
+
+    }
+
+}
