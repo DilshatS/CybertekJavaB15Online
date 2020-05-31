3315dec6695c5f7cdde2ad3163a08374cd5ec766
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-30 23:01:47 -0700

    day 29: String Practice Hide Hero Identity Skinny version

---
+++ src/day29/HeroIdentity_Skinny.java
@@ -0,0 +1,33 @@
+package day29;
+
+public class HeroIdentity_Skinny {
+
+    public static void main(String[] args) {
+
+        // assume this can be different hero
+        String hero1 = "Superman-Clark J Kent";
+        // hide this hero identity
+
+        //String heroX = "Superman-************";
+
+        // given a String with hero code and name separated by -
+        // turn this String into hero code and * with same length as hero name
+
+        // Plain English Logic :
+
+        // split it by - to get the code and full name
+        // get the length of full name
+        // generate starts with same length as full name character count
+        // OR one the star is generated , replace full name with stars
+
+        String stars = "";
+        for (int i = 1; i <= hero1.split("-")[1].length(); i++)
+
+            stars += "*";
+
+        System.out.println("hero1 = " + hero1);
+        System.out.println("heroX = " + hero1.replace(hero1.split("-")[1], stars));
+
+    }
+
+}
