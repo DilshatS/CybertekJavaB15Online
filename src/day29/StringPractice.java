093de747b4abe9eb092af509ef7f4e5b3647af23
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-05-30 21:57:40 -0700

    day 29: String Practice split and initial practice

--- src/day29/StringPractice.java
+++ src/day29/StringPractice.java
@@ -16,12 +16,12 @@ public class StringPractice {
 
 //        String newHero = hero1.replace("-"," ");
 //        System.out.println("hero1 = " + hero1);
-        String[] heroSplitted = hero1.split("-");
-        System.out.println("heroSplitted = " + Arrays.toString(heroSplitted));
-        String heroCode = heroSplitted[0];
-        String fullName = heroSplitted[1];
+        String[] heroSplit = hero1.split("-");
+        System.out.println("heroSplitted = " + Arrays.toString(heroSplit));
+        String heroCode = heroSplit[0];
+        String fullName = heroSplit[1];
 
-        System.out.println(heroSplitted[0]);
+        System.out.println(heroSplit[0]);
 
         // Hero core is Superman and Identity is Clark Kent
         //System.out.println("Hero code is " + heroSplitted[0] + " and Identity is " + heroSplitted[1]);
@@ -33,8 +33,26 @@ public class StringPractice {
         // LOGIC1 : get the first letter of full name then get the letter after the space
         //          then get first letter after space
         //          concatenate them
+        //          downside is what if we have middle name
+        //          solution, instead of looking for the letter after first space
+        //          look for last index of the space then get next Character
+
+        // LOGIC2 : We already have full name stored in fullName variable
+        //          split by space --> we will get firstName (middle name) lastName
+        //          get the first letter of full name
+        //          then get the last name using last item index
+        //          get the first character then concatenate
+        //          Clark Kent -> [Clark, Kent] Clark -> C , Kent -> K --> CK
 
 
+        String[] fullNameSplit = fullName.split(" ");
+        System.out.println("fullNameSplit = " + Arrays.toString(fullNameSplit));
+        String heroFirstName = fullNameSplit[0];
+        // last name will be always the last element by splitting full name
+        // no matter the name has middle name or not
+        String heroLastName = fullNameSplit[fullNameSplit.length-1];
+
+        System.out.println("Hero Initials : "+ heroFirstName.charAt(0) + heroLastName.charAt(0));
 
     }
 
