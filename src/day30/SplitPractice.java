21b6ac3a6d811c4297961f2bcc07d7ac3c0172cc
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-01 13:09:31 -0700

    day 30: Array Practice Split Method to correct model issue

--- src/day30/SplitPractice.java
+++ src/day30/SplitPractice.java
@@ -6,9 +6,24 @@ public class SplitPractice {
 
     public static void main(String[] args) {
 
+        /*
+        What does split do?
+        it will split the String into multiple part a
+
+        What do I need to provide when calling it?
+        We need to provide the separator
+
+        What do I get out of it
+        We get a String array out of it with each prat split
+
+        Does it actually change your ORIGINAL String
+        NO!!!
+         */
+
         String car = "Lexus-IS-F";
         String[]carSplit = car.split("-");
         System.out.println("carSplit = " + Arrays.toString(carSplit));
+        System.out.println("car = " + car);
 
         String model = carSplit[1];
         System.out.println("model = " + model);
