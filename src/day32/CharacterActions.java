d3a4e3c6229a30ae79c731c6b48424ee806dd9a7
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-06 23:12:41 -0700

    day32: void method 2 parameters print alphabet in range

--- src/day32/CharacterActions.java
+++ src/day32/CharacterActions.java
@@ -4,9 +4,43 @@ public class CharacterActions {
 
     public static void main(String[] args) {
 
-        printAtoZ('A');
-        printZtoA();
-        printAtoZ('D');
+//        printAtoZ('A');
+//        printZtoA();
+//        printAtoZ('D');
+        printAlphabetInRange('a', 'f');
+        printAlphabetInRange('G', 'B');
+        printAlphabetInRange('A','a');
+        printAlphabetInRange('Z', 'a');
+
+    }
+
+    // if beginning character is before ending character
+    // for example beginning A , ending D  -->> ABCD
+    // we did similar action with printing A-Z
+    // <p>
+    // if beginning character is after ending character
+    // for example beginning D , ending A  -->> DCBA
+    // we did similar action with printing Z-A
+
+    public static void printAlphabetInRange (char beginning, char ending){
+
+        if ( beginning < ending ){
+            System.out.println("WE NEED TO INCREMENT FROM " + beginning + " till " + ending);
+
+            for(char iCHar = beginning; iCHar <= ending; iCHar++){
+                System.out.print(iCHar + " ");
+            }
+            System.out.println();
+        }else if ( beginning > ending ){
+            System.out.println("WE NEED TO DECREMENT FROM " + beginning + " till " + ending);
+
+            for (char iChar = beginning; iChar >= ending; iChar--){
+                System.out.print(iChar + " ");
+            }
+            System.out.println();
+        }else {
+            System.out.println("THEY ARE SAME CHARACTER!!!");
+        }
 
     }
 
