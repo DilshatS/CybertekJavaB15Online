d2da9f0375e4f640f9e37eb376965dca2f84ddc0
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-08 18:54:36 -0700

    day33: Generate NightWatch.com email from first and last name

--- src/day33/NumberActions.java
+++ src/day33/NumberActions.java
@@ -9,8 +9,26 @@ public class NumberActions {
      */
 
     public static void main(String[] args) {
-
+        // 1+2+3+4+5+6+7 = 28
         System.out.println(getSumFrom1toX(7));
+
+        System.out.println(build_GOT_Gmail("Dilshat", "Sadamov"));
+        System.out.println(build_GOT_Gmail("Sam", "Tarly"));
+
+        // you are given a variable fullName that has first name and last name
+        // use this full name to generate GOT_email
+
+        String fullName = "Susan Cure";
+//        store this resulting email
+//        String part1 = fullName.split(" ")[0];
+//        String part2 = fullName.split(" ")[1];
+//        String herEmail = build_GOT_Gmail(part1,part2);
+
+
+        String[] fullNameSplit = fullName.split(" ");
+        String herEmail = build_GOT_Gmail(fullNameSplit[0], fullNameSplit[1]);
+
+        System.out.println("herEmail = " + herEmail);
 
     }
 
@@ -36,10 +54,19 @@ public class NumberActions {
     /**
      * build_GOT_Gmail
      * This method should build email using user's full name
-     * for example Jon Snow -> JSnow@NughWatch.com
+     * for example Jon Snow -> JSnow@NighWatch.com
      * @param firstName user's first name
      * @param lastName user's last name
      * @return the email created using
      * firstName initial+lastName+@NughWatch.com
      */
+
+    public static String build_GOT_Gmail(String firstName, String lastName){
+
+        String domain = "@NighWatch.com";
+        String email = firstName.charAt(0) + lastName + domain;
+
+        return email;
+
+    }
 }
