665eb5c9f768a0770e2611ab074ed2b881937797
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-16 00:43:07 -0700

    day36: ArrayList Methods intro

--- src/day36/ArrayListMethod.java
+++ src/day36/ArrayListMethod.java
@@ -1,4 +1,36 @@
 package day36;
 
+import java.util.ArrayList;
+
 public class ArrayListMethod {
+
+    public static void main(String[] args) {
+
+        // create a ArrayList Object of long and assign it to a variable
+
+        ArrayList<Long> lst = new ArrayList<>();
+
+//        C.R.U.D
+//                Create, Read, Update, Delete
+
+        // add item, insert an item, read item, update the item, remove the item, check the location ...
+
+        lst.add( 12L  ); // 12L is automatically converted to new Long(12L) ; because ArrayList only store object
+        lst.add( 100L );
+        lst.add( 120L );
+        lst.add( 150L );
+        lst.add( 200L );
+
+        System.out.println("lst = " + lst);
+
+        // Counting items inside ArrayList
+        System.out.println("lst.size() = " + lst.size());
+
+        // Getting items inside ArrayList object
+        System.out.println("First item is : lst.get(0) = " + lst.get(0));
+
+        // TASK : GET THE SUM OF ABOVE ARRAY LIST ITEMS
+
+    }
+
 }
