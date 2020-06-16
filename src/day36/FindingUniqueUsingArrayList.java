7d338968291eee4de2e37a49a52b15a750fe27e6
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-16 15:20:05 -0700

    day36: Finding unique items in array using ArrayList and contains method

--- src/day36/FindingUniqueUsingArrayList.java
+++ src/day36/FindingUniqueUsingArrayList.java
@@ -1,11 +1,26 @@
 package day36;
 
+import java.util.ArrayList;
+
 public class FindingUniqueUsingArrayList {
 
     public static void main(String[] args) {
 
+        int[] nums = {11,33,44,11,33,44,22,22,55,44,33};
 
+        ArrayList<Integer> uniqueLst = new ArrayList<>();
 
+        // I want to add items to uniqueLst if it does not already contains that item
+
+        for (int each : nums){
+
+            //System.out.println("each = " + each);
+            if ( ! uniqueLst.contains( each) )
+            uniqueLst.add(each);
+
+        }
+
+        System.out.println("uniqueLst = " + uniqueLst);
     }
 
 }
