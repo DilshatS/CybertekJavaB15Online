2d2d79be2978a93e50ccf83abfdafbdaadd648cf
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-19 22:55:29 -0700

    day38: Creating List and ArrayList object in one short, and copying values

--- src/day38/ArrayListPractice_ShortWay.java
+++ src/day38/ArrayListPractice_ShortWay.java
@@ -1,9 +1,8 @@
-package day37;
+package day38;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ArrayListPractice_ShortWay {
 
