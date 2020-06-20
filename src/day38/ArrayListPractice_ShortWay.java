051cbb202bc30651cc4a77f0a5e3211825a693c9
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-19 22:45:04 -0700

    day38: Creating List and ArrayList object in one short, and copying values

---
+++ src/day38/ArrayListPractice_ShortWay.java
@@ -0,0 +1,49 @@
+package day38;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ArrayListPractice_ShortWay {
+
+    public static void main(String[] args) {
+
+        // create List of 6 double in short way,
+        // we will just read them and will not add or remove item
+
+        List<Double> price = Arrays.asList(9.99, 5.99, 5.55, 3.76, 8.99, 0.99, 65.67, 0.99);
+        System.out.println("price = " + price);
+        //price.add(12.8);
+        //price.remove(0);
+        int count = 0;
+        for (Double each : price) {
+            if (each > 5){
+                ++count;
+            }
+        }
+        System.out.println("count = " + count);
+
+        // I want to add 2 more prices ( does not have to be same list )
+        ArrayList<Double> newPrices = new ArrayList<>( price );
+        newPrices.add(199.99);
+        newPrices.remove(0.99); // remove method will remove first occurrence of the value
+        System.out.println("newPrices = " + newPrices);
+
+        // Create a ArrayList Object in one short with many items so we can add or remove
+
+        // Just one option to create ArrayList Object in one short
+        ArrayList<Double> oneShotPrice =
+                new ArrayList<>( Arrays.asList(9.99, 5.55, 3.76, 8.99) );
+        System.out.println("oneShotPrice before = " + oneShotPrice);
+
+        // Add 100.99 after 9.99
+        oneShotPrice.add(1, 100.99);
+        System.out.println("oneShotPrice after adding 100.99 = " + oneShotPrice);
+        // remove item 3
+        oneShotPrice.remove(2);
+        System.out.println("oneShotPrice after removing 3rd item = " + oneShotPrice);
+
+    }
+
+}
