44d4118a7a14d8de25d3ce175a00352a3c1f7df3
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-20 22:49:49 -0700

    day38: RetainAll Method Keep the common part

---
+++ src/day38/RetailAllPractice.java
@@ -0,0 +1,33 @@
+package day38;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Retains only the elements in this list that are contained in the specified collection.
+ * In other words,
+ * removes from this list all of its elements that are not contained in the specified collection.
+ */
+public class RetailAllPractice {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> nums1 = new ArrayList<>();
+        nums1.add(100);
+        nums1.add(200);
+        nums1.add(300);
+        nums1.add(400);
+        nums1.add(500);
+        nums1.add(600);
+        System.out.println("nums1 = " + nums1);
+
+        List<Integer> nums2 = new ArrayList<>(Arrays.asList(200,300,600,700,800));
+        System.out.println("nums2 = " + nums2);
+
+        nums1.retainAll(nums2);
+        System.out.println("nums1 = " + nums1);
+
+    }
+
+}
