13a8f423e565d989e34b9a9438c282374ccf7cfc
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-20 20:45:18 -0700

    day38: Add all methods for Shopping List

--- src/day38/ArrayListBulkOperation.java
+++ src/day38/ArrayListBulkOperation.java
@@ -45,6 +45,12 @@ public class ArrayListBulkOperation {
         System.out.println("nums1 = " + nums1);
         System.out.println("nums2 = " + nums2);
 
+        // Arrays.asList method will return a List Object that contains items it specified
+        // add 4 items to nums2 using add all 100, 200, 300, 400
+
+        nums2.addAll(Arrays.asList(100,200,300,400));
+        System.out.println("nums2 after adding 4 items = " + nums2);
+
     }
 
 }
