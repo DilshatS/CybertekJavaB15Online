db83b2577cea9aa7cf857515d755ca228ef4d064
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-22 14:40:06 -0700

    day39: Split Practice : Updating price and monthly payment in Item details String

--- src/day39/SplitPractice.java
+++ src/day39/SplitPractice.java
@@ -4,21 +4,25 @@ public class SplitPractice {
 
     public static void main(String[] args) {
 
-        String itemDetails = "iPhone 6s,449,18.71";
+        String itemDetails = "Dyson Vacuum,399,16.25";
+        System.out.println("itemDetails before = " + itemDetails);
         String[] itemSplitArr = itemDetails.split(",");
 
         String name = itemSplitArr[0];
-        System.out.println("name = " + name);
+//        System.out.println("name = " + name);
 
         double price = Double.parseDouble( itemSplitArr[1] );
-        System.out.println("price = " + price);
+//        System.out.println("price = " + price);
 
         double monthly = Double.parseDouble(itemSplitArr[2]);
-        System.out.println("monthly = " + monthly);
+//        System.out.println("monthly = " + monthly);
 
-        System.out.println("item name = " + itemDetails.split(",")[0]);
-        System.out.println("item price = " +  Double.parseDouble(itemDetails.split(",")[1]));
-        System.out.println("item monthly = " + Double.parseDouble(itemDetails.split(",")[2]));
+        itemDetails = name + "," + price*0.2 + "," + monthly*0.2;
+        System.out.println("itemDetails after = " + itemDetails);
+
+//        System.out.println("item name = " + itemDetails.split(",")[0]);
+//        System.out.println("item price = " +  Double.parseDouble(itemDetails.split(",")[1]));
+//        System.out.println("item monthly = " + Double.parseDouble(itemDetails.split(",")[2]));
 
 //        System.out.println("item name = " + itemDetails.split(",")[0]);
 //        System.out.println("item price = " + itemDetails.split(",")[1]);
