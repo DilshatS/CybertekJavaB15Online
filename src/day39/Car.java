1a20cb0edb5078037dc55af015676fb546e3674c
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-23 21:49:46 -0700

    day39: Car clss and car objects | instance methods with parameters

---
+++ src/day39/Car.java
@@ -0,0 +1,14 @@
+package day39;
+
+public class Car {
+
+    // year, model, make, color
+    int year;
+    String model;
+    String make;
+    String color;
+
+    // or in one shot
+    //String model, make, color;
+
+}
