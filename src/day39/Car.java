e92adabe0fc11bf4964f3095e0f18da676d1ad8c
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-24 00:25:23 -0700

    day39: Car class and car objects | instance methods with parameters

--- src/day39/Car.java
+++ src/day39/Car.java
@@ -11,4 +11,22 @@ public class Car {
     // or in one shot
     //String model, make, color;
 
+    public void goForward(){
+
+        System.out.println(make + " Going forward");
+
+    }
+
+    // Create a method to print how old is this car
+    public void printAge(){
+
+        System.out.println("car year : " + year);
+        System.out.println("car age : " + (2020 - year));
+
+    }
+
+    // instance methods are tied to the object
+    // it does not have static keyword
+    // we need an object when we call them
+
 }
