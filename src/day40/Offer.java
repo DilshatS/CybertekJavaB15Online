f405cf80919b85ddaee6a37a91127d95f4b912b2
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-24 01:43:16 -0700

    day40: Custom classes | Offer Task

---
+++ src/day40/Offer.java
@@ -0,0 +1,22 @@
+package day40;
+
+public class Offer {
+
+    String location;
+    String company;
+    long salary;
+    boolean isFullTime;
+
+    /**
+     * This is instance method to print all the information about Offer object
+     * no parameter and no return type
+     */
+    // inside instance method we can directly access instance variable
+    public void displayInformation(){
+
+        System.out.println("Location + " + location + " | " + "Company = " + company + " | "
+                            + "Salary = " + salary + "$ | " + "isFullTime" + isFullTime);
+
+    }
+
+}
