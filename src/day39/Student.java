4388125fbc31e711673632630349357f0527be03
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-23 17:52:33 -0700

    day39: Custom Classes | instance field/variable

---
+++ src/day39/Student.java
@@ -0,0 +1,21 @@
+package day39;
+
+    /*
+    Any time you create a class, it will become a blueprint for object
+    And this class type can be a type for variable
+    for example String has it's own class so we can create a String variable
+    and assign a String object
+
+    in here student become a blue print for creating student object '
+    and any student object has 3 attributes defined by instance field|variable
+     */
+public class Student {
+
+    // instance field | variable :
+    String name ;
+    int age;
+    char gender;
+
+
+
+}
