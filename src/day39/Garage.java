5a7bd8c9b04b74cea97ab0b986e0d1a4c873d984
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-24 00:31:01 -0700

    day39: Car class and car objects | instance methods

--- src/day39/Garage.java
+++ src/day39/Garage.java
@@ -4,34 +4,37 @@ public class Garage {
 
     public static void main(String[] args) {
 
-        Car car1 = new Car();
+        // Create car object using Class
+        // or
+        // Get an instance of Car class
 
-        car1.model = "4 Runner";
-        car1.color = "White";
+        Car car1 = new Car();
         car1.make = "Toyota";
+        car1.model = "4 Runner";
+        car1.color = "white";
         car1.year = 2020;
+        car1.goForward();
 
         System.out.println("car1.make = " + car1.make);
         System.out.println("car1.year = " + car1.year);
 
-        System.out.println("---------");
+        System.out.println("-----------");
 
         Car car2 = new Car();
-
-        car2.model = "SantaFe";
-        car2.color = "Black";
-        car2.make = "Hyundai";
         car2.year = 2017;
+        car2.make = "Hyundai";
+        car2.model = "SantaFe";
+        car2.color = "black";
+        car2.goForward();
+        System.out.println("car2.make = " + car2.model);
+        System.out.println("car2.color = " + car2.color);
 
-        System.out.println("car1.model = " + car2.model);
-        System.out.println("car1.color = " + car2.color);
-        
         car2.year = 2020;
-        System.out.println("car2.year = " + car2.year);
+        System.out.println("car2 year = " + car2.year);
 
-        // I WANT TO CHANGE THE COLOR OF HYUNDAI TO THE COLOR OF TOYOTA
-        car2.color = car1.color;
-        System.out.println("car2.color = " + car2.color); // car2.color = "white";
+        // I WANT TO CHANGE THE COLOR OF Hyundai TO THE COLOR OF Toyota
+        car2.color = car1.color;  //car2.color = "white" ;
+
 
     }
 
