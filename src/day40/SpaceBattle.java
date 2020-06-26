8ebb5daf9610dcf8fab013330d2ef6e303ef60b3
Dilshat <37429878+DilshatS@users.noreply.github.com>
2020-06-25 22:07:57 -0700

    day40: Main Class for SpaceShip

--- src/day40/SpaceBattle.java
+++ src/day40/SpaceBattle.java
@@ -8,12 +8,26 @@ public class SpaceBattle {
         s1.name = "Orville";
 
         s1.setInitialPosition(1,5);
-        s1.setDirection("ABC");
-        s1.move1Block();
-        System.out.println("s1 = " + s1);
 
-        s1.setDirection("abv");
-        s1.move1Block();
+//        // IF GIVEN WRONG IT WILL NOT MOVE!!!
+//        s1.setDirection("ABC");
+//        s1.move1Block();
+//        System.out.println("s1 = " + s1);
+//
+//        // IF GIVEN WRONG IT WILL NOT MOVE!!!
+//        s1.setDirection("abv");
+//        s1.move1Block();
+//        System.out.println("s1 = " + s1);
+//
+//        s1.setDirection("down");
+//        s1.move1Block();
+//        System.out.println("s1 = " + s1);
+
+        s1.move1Block("up");
+        s1.move1Block("right");
+        s1.move1Block("up");
+        s1.move1Block("down");
+        s1.move1Block("Home");
 
         System.out.println("s1 = " + s1);
 
