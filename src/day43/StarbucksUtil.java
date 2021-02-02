914e89c18679e43800af6fd9ddc55c6871036a85
Dilshat <37429878+DilshatS@users.noreply.github.com>
2021-02-02 09:53:00 -0800

    day 43: Starbucks util

---
+++ src/day43/StarbucksUtil.java
@@ -0,0 +1,49 @@
+package day43;
+
+public class StarbucksUtil {
+
+    public static void main(String[] args) {
+
+        Coffee c = new Coffee("Blonde",5, 1.6);
+        printCoffeeInfo(c);
+
+        String myString = new String("Hello");
+        printStringInfo(myString);
+
+        // we would need and object of current class if we wanted call
+        // instance method of current class
+//        StarbucksUtil util = new StarbucksUtil();
+//        util.printStringInfo(myString);
+
+    }
+
+    // Create a static method to accept a coffee object and print IT'S information
+    // like this: This coffee is : name, price is <price>, caffeine level is <level>
+
+    /**
+     * Create a static method to accept a coffee object and print IT'S information
+     * like this: This coffee is : name, price is <price>, caffeine level is <level>
+     * @param co the coffee object caller pass
+     */
+
+    public static void printCoffeeInfo(Coffee co){
+
+        System.out.println("This coffee is : " + co.getType());
+        System.out.println("Price is : " + co.getPrice());
+        System.out.println("CaffeineLevel is : " + co.getCaffeineLevel());
+
+    }
+
+    /**
+     * a static method to print out first and last character of a String
+     * @param str
+     */
+
+    public static void printStringInfo(String str){
+
+        System.out.println("first character is : " + str.charAt(0));
+        System.out.println("last character is : " + str.charAt(str.length()-1));
+
+    }
+
+}
