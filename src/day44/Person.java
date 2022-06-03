a23cf18115bdfc0c42837dae4026422db461d07f
Dilshat <37429878+DilshatS@users.noreply.github.com>
2022-06-03 16:12:16 -0700

    day 44: Person class, static field added

--- src/day44/Person.java
+++ src/day44/Person.java
@@ -1,9 +1,15 @@
 package day44;
 
 public class Person {
-
+    // instance field: belong to the instance(Object)
+    // every Person object will get their own name and age
+    // changing the value of one object does not affect another
     private String name;
     private int age;
+    // static field: belong to the class (type itself)
+    // there is only one copy of this value shared by all the objects
+    // created out of this Person template/ blue print
+    public static String species;
 
     public Person(String name, int age){
         this.name = name;
@@ -17,11 +23,11 @@ public class Person {
         return age;
     }
 
-    @Override
     public String toString() {
         return "Person{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
+                ", species='" + species + '\'' +
                 '}';
     }
 }
